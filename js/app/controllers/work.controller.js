48aa938122e1dfc9b6a2ddb65ea1376d9f3423a2
myunghoon kim <myungtkim@gmail.com>
2016-04-16 20:47:40 -0700

    adds  to data binding.

--- js/app/controllers/work.controller.js
+++ js/app/controllers/work.controller.js
@@ -5,11 +5,17 @@
     .module("Homme")
     .controller("WorkController", WorkController);
 
-  WorkController.$inject = ["$log", "$state"];
+  WorkController.$inject = ["$log", "$state", "$rootScope"];
 
-  function WorkController($log, $state) {
+  function WorkController($log, $state, $rootScope) {
     $log.debug("Work Controller loaded");
     var vm = this;
+    vm.$state = $state;
+
+    $rootScope.$on("$viewContentLoaded", function(evt) {
+      $log.debug("state change detected", evt)
+      $log.debug($state.current.name)
+    })
 
   };
 
