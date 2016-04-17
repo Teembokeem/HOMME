ffee593f659780e7a7233db1677140ce255022b7
myunghoon kim <myungtkim@gmail.com>
2016-04-16 21:18:11 -0700

    adds basic templating for main controller for the landing and home page

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,3 +1,18 @@
 (function() {
+  'use strict'
+
+  angular
+    .module("Homme")
+    .controller("MainController", MainController);
+
+  MainController.$inject = ["$log", "$state"];
+
+  function MainController($log, $state) {
+    $log.debug("MainController loaded.");
+
+    var vm = this;
+
+    vm.$state = $state;
+  }
 
 })();
